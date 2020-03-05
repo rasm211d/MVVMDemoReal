8672e4654314106238b013a880dd7e8afbbe747b
Rasmus Jannerup <rasm211d@edu.easj.dk>
2020-03-05 21:40:43 +0100

    Step 2 er done.

--- app/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.mvvmdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.editText);
+        final TextView textView = findViewById(R.id.textView);
+        final Button button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText());
+            }
+        });
+
     }
 }
